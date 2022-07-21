9b0c7935fddff6052448ba1ddeb3764e4335a47c
HarshKumar-212 <harsh_kumar@persistent.com>
2022-07-21 12:04:37 +0530

    Eureka Server Added

--- Order_Management/src/main/java/com/psl/alp/ordermanagement/OrderManagementApplication.java
+++ Order_Management/src/main/java/com/psl/alp/ordermanagement/OrderManagementApplication.java
@@ -2,10 +2,12 @@ package com.psl.alp.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.psl.alp.FeignClients")
+@EnableEurekaClient
 public class OrderManagementApplication {
 
 	public static void main(String[] args) {
