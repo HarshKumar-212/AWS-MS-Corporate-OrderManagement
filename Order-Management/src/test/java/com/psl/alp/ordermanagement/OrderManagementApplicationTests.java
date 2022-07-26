5b04f09f5c8ae89408e84d8e7260b1a35d4fa041
HarshKumar-212 <harsh_kumar@persistent.com>
2022-07-26 10:47:47 +0530

    Changes

--- Order-Management/src/test/java/com/psl/alp/ordermanagement/OrderManagementApplicationTests.java
+++ Order-Management/src/test/java/com/psl/alp/ordermanagement/OrderManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.psl.alp.ordermanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = OrderManagementApplicationTests.class )
+@SpringBootTest(classes = OrderManagementApplicationTests.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderManagementApplicationTests {
 
 	@Test
